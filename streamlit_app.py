c7c949874b431e37b3401b631712d2e69a0ec6b8
tlyons66 <107857236+tlyons66@users.noreply.github.com>
2022-06-20 20:32:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,10 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍟 Omega 3 & Blueberry Oatmeal')
 
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 
-streamlit.text('Hard-Boiled Free Range Egg')
+streamlit.text('🥚 Hard-Boiled Free Range Egg')
+
+streamlit.text('🥑🧇 Avacado Toast')
